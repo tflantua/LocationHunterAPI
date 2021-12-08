3383f97dc300442405e75b5965520f598bd505cf
Thomas <thomas.flantua@gmail.com>
2021-12-08 14:40:37 +0100

    test for login

--- www/api/requests/loginRequest.php
+++ www/api/requests/loginRequest.php
@@ -4,15 +4,15 @@ include '../../Status.php';
 include '../../StatusMessage.php';
 
 if (!empty($_POST['userName']) && !empty($_POST['password'])) {
-    $userName = htmlspecialchars($_POST["userName"], ENT_QUOTES, "UTF-8");
+    $userName = htmlspecialchars($_POST["userName"], ENT_QUOTES);
     $userName = filter_var($userName, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-    $password = htmlspecialchars($_POST["password"], ENT_QUOTES, "UTF-8");
+    $password = htmlspecialchars($_POST["password"], ENT_QUOTES);
     $password = filter_var($password, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
 
     //Function
     $login = new Login($userName, $password);
-//    var_dump($login);
-//    $login->logIn();
+    echo 'yes';
+    $login->logIn();
 } else {
     $status = new Status();
     $status->isEmpty();
