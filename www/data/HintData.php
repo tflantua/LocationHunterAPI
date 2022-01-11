482e545af8dabdb75f6e4481e1c69bcce17489dc
Thomas <thomas.flantua@gmail.com>
2022-01-11 21:42:49 +0100

    De functie om de locaties op te halen kan gedeeltelijk getest worden.

---
+++ www/data/HintData.php
@@ -0,0 +1,58 @@
+<?php
+
+class HintData
+{
+    public $hint;
+    public $ID;
+    public $cost;
+
+    /**
+     * @return mixed
+     */
+    public function getHint()
+    {
+        return $this->hint;
+    }
+
+    /**
+     * @param mixed $hint
+     */
+    public function setHint($hint)
+    {
+        $this->hint = $hint;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getID()
+    {
+        return $this->ID;
+    }
+
+    /**
+     * @param mixed $ID
+     */
+    public function setID($ID)
+    {
+        $this->ID = $ID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * @param mixed $cost
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+    }
+
+
+}
--- www/data/LocationData.php
+++ www/data/LocationData.php
@@ -12,11 +12,30 @@ class LocationData
     public $hint;
     public $hintId;
     public $cost;
+    public $hintsList;
 
-    public function set($data){
-        var_dump($data);
-        foreach ($data AS $key => $value) $this->{$key} = $value;
+    public function set($data)
+    {
+        foreach ($data as $key => $value) $this->{$key} = $value;
+
+        $this->setHints();
     }
 
+    private function setHints()
+    {
+        $explodeHints = explode(";", $this->hint);
+        $explodeHintIds = explode(";", $this->hintId);
+        $explodeCosts = explode(";", $this->cost);
 
+        for ($i = 0; $i < sizeof($explodeHints); $i++) {
+            $hintData = new HintData();
+            $hintData->setHint($explodeHints[$i]);
+            $hintData->setCost($explodeCosts[$i]);
+            $hintData->setID($explodeHintIds[$i]);
+
+            $this->hintsList[$i] = $hintData;
+        }
+
+        var_dump($this->hintsList);
+    }
 }
