5a050665e9bf7a6ac86b7dc0455306a55e18b96d
Thomas <thomas.flantua@gmail.com>
2022-01-14 15:29:34 +0100

    De functie om de locaties op te halen kan gedeeltelijk getest worden.

--- www/LocationUtil/Locations.php
+++ www/LocationUtil/Locations.php
@@ -17,7 +17,7 @@ class Locations
         if ($this->conn != null) {
             $userId = CheckKey::check($this->key, $this->conn);
             if ($userId != null) {
-                $query = "SELECT  
+                $query = "SELECT all 
 `North` as 'north', `East` as 'east', `Name` as 'name', `RiddleName` as 'riddleName',
 `Riddle` as 'riddle', `Points` as 'points', `Difficulty` as 'difficulty', GROUP_CONCAT(`Hint` SEPARATOR ';') as 'hint', group_concat(`HintsID` SEPARATOR ';') as hintId, group_concat(`Cost` SEPARATOR ';') as cost
                             FROM Location INNER JOIN Hints ON Location.ID = Hints.LocationID GROUP BY `North`, `East`, `Name`, `RiddleName`, `Riddle`, `Points`, `Difficulty`";
