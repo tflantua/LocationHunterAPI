d99aa4976ed1e68575af6be3a6c18c659d6d92d2
Thomas <thomas.flantua@gmail.com>
2022-01-14 15:56:10 +0100

    De functie om de locaties op te halen kan gedeeltelijk getest worden.

--- www/LocationUtil/Locations.php
+++ www/LocationUtil/Locations.php
@@ -25,7 +25,7 @@ class Locations
                 if ($result) {
 //                    $info = mysqli_fetch_all($result);
 //                    var_dump($info);
-                    var_dump(mysqli_stmt_next_result($result));
+                    var_dump($result);
                     $locationList = [];
 //                    for ($i = 0; $i < sizeof($info); $i++) {
 //                        $location = $info[$i];
